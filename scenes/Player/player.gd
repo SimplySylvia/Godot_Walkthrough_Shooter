a5cff91a30a4ed1ad8c9a5e34bfb286fe20a9f20
Sylvia Hart <sylvia.the.dev@gmail.com>
2023-09-29 13:17:08 -0700

    adds lazer and grenade base spawn logic

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-signal lazer
-signal grenade
+signal lazer(pos)
+signal grenade(pos)
 
 var can_lazer: bool = true
 var can_grenade: bool = true
@@ -15,15 +15,21 @@ func _process(_delta):
 	
 	# lazer shooting input 
 	if Input.is_action_pressed("primary action") and can_lazer:
-		lazer.emit()
+		#randomly select a marker for the lazer starting position
+		var lazer_markers = $LazerStartPositions.get_children()
+		var selected_lazer = lazer_markers[randi() % lazer_markers.size()]
 		can_lazer = false
 		$Timer.start()
+		#emit the position we selected
+		lazer.emit(selected_lazer.global_position)
 
 	# throw grenade 
 	if Input.is_action_pressed("secondary action") and can_grenade:
-		grenade.emit()
+		var grenade_markers = $GrenadeStartPositions.get_children()
+		var selected_grenade = grenade_markers[randi() % grenade_markers.size()]
 		can_grenade = false
 		$GrenadeReloadTimer.start()
+		grenade.emit(selected_grenade.global_position)
 
 
 # signal from timer for when it completed
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
+# preloads the scene 
+var lazer_scene: PackedScene = preload("res://scenes/Projectiles/lazer.tscn")
+var grenade_scene: PackedScene = preload("res://scenes/Projectiles/grenade.tscn")
+
 func _on_gate_player_entered_gate(body):
 	print("player has entered gate")
 	print(body)
 
-func _on_player_lazer():
-	print("lazer from level")
+func _on_player_lazer(pos):
+	# create an instance of the lazer scene
+	var lazer = lazer_scene.instantiate()
+	# update lazer position
+	lazer.position = pos
+	# add the lazer instance to a node2d and put lazer into current scene
+	$Projectiles.add_child(lazer)
 
-func _on_player_grenade():
-	print("grenade from level")
+func _on_player_grenade(pos):
+	var grenade = grenade_scene.instantiate()
+	grenade.position = pos
+	$Projectiles.add_child(grenade)
