6eb35a0e53539f0e412aca87e21c112d72e8aec4
Sylvia Hart <sylvia.the.dev@gmail.com>
2023-09-29 10:06:08 -0700

    adds base of signals for scene connection

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -1,22 +1,35 @@
 extends CharacterBody2D
 
+signal lazer
+signal grenade
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass 
-
+var can_lazer: bool = true
+var can_grenade: bool = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	# get input
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * 500
 	move_and_slide()
 	
 	# lazer shooting input 
-	if Input.is_action_pressed("primary action"):
-		print("shoot lazer")
+	if Input.is_action_pressed("primary action") and can_lazer:
+		lazer.emit()
+		can_lazer = false
+		$Timer.start()
 
 	# throw grenade 
-	if Input.is_action_pressed("secondary action"):
-		print("throw grenade")
+	if Input.is_action_pressed("secondary action") and can_grenade:
+		grenade.emit()
+		can_grenade = false
+		$GrenadeReloadTimer.start()
+
+
+# signal from timer for when it completed
+func _on_timer_timeout():
+	can_lazer = true
+
+# signal from timer for when it completed
+func _on_grenade_reload_timer_timeout():
+	can_grenade = true
--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+func _on_gate_player_entered_gate(body):
+	print("player has entered gate")
+	print(body)
+
+func _on_player_lazer():
+	print("lazer from level")
+
+func _on_player_grenade():
+	print("grenade from level")
